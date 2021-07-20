cdf1e825ec2346b4cb58762c2cbb84beb57389d6
Gabriel Wolf <gabrielfai70@gmail.com>
2021-07-19 22:36:09 -0300

    Alteração no título da página

--- app/sts/View/include/cabecalho.php
+++ app/sts/View/include/cabecalho.php
@@ -5,6 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" href="<?php echo URL; ?>assets/imagens/icone/favicon.ico">
-    <title>Document</title>
+    <title>Controle de Ponto</title>
 </head>
 <body>
